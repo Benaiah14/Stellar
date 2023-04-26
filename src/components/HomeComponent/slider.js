65541f503c8942d5efad354e77c659b4d80428d3
Benaiah <benaiah.abolaji@gmail.com>
2023-04-26 21:17:13 +0100

    df

--- src/components/HomeComponent/slider.js
+++ src/components/HomeComponent/slider.js
@@ -4,7 +4,7 @@ import '../../assets/css/pages/Home/slider.css'
 const Slider = () => {
   return (
     <header className='slider'>
-        <img src={require('../../assets/images/Home/header/model.png')} className='img'/>
+        <img src={require('../../assets/images/Home/header/model.PNG')} className='img'/>
       <div>
         <h1 className='level'>Level Up Your <br />Style with our <br />Summer <br />Collections</h1>
         <a className='shopNow' href='/shop'>Shop Now <IoMdArrowForward /></a>
